1ed121241ff2918aaa3b8df76edc1106a5d9ac9b
JP Meijers <gitkraken@jpmeijers.com>
2018-09-08 16:53:24 +0200

    Env var didn't expand correctly

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ if [ -z "$NGINX_URL" ]; then
   echo "No NGINX_URL, using https://192.168.8.100/"; 
 else
   echo "NGINX_URL is set to '$NGINX_URL'"; 
-  sed -i -e 's/https:\/\/192.168.8.100\//$NGINX_URL/g' /etc/nginx/nginx.conf
+  sed -i -e "s@https:\/\/192.168.8.100\/@$NGINX_URL@g" /etc/nginx/nginx.conf
 fi
 cat /etc/nginx/nginx.conf
 nginx -g "daemon off;"
